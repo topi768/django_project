6be615c395feee5abaa8f3ecd36b6edf4a83c058
topi768 <topi768@inbox.ru>
2024-10-12 08:09:38 +1000

    delete comments

--- project/my_app/views.py
+++ project/my_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, home!")
+
+def say_hallo(request):
+    return render(request, "hello.html", {'name': 'vasya'})
