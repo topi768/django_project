cf76a67607bd3f9ef555864964d9743e4f72490f
topi768 <topi768@inbox.ru>
2024-11-09 08:28:43 +1000

    1

--- project/my_app/views.py
+++ project/my_app/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     return HttpResponse("Hello, home!")
+
 
 def say_hallo(request):
     return render(request, "hello.html", {'name': 'vasya'})
