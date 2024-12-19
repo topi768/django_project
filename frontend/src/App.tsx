ba108b912ad5064d6933336814475a89646febf0
topi768 <topi768@inbox.ru>
2024-12-19 19:17:52 +1000

    Последний коммит на новом бранче

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import Register from './pages/RegisterPage';
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+    <Register/>
+    <div className='bg-blue-500 text-white p-4' >dawdadwadw</div>
+    </>
+  )
+}
+
+export default App
