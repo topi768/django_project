2a6149bf9ce341bd1a92fa60ff72e743a9e71ecb
topi768 <topi768@inbox.ru>
2025-01-22 19:13:11 +1000

    add test

--- django_project/project/new_app/tests.py
+++ django_project/project/new_app/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from colorama import Fore, Back, Style, init
 
-# Create your tests here.
+# Инициализируем colorama
+init(autoreset=True)
+
+class UserRegistrationTestCase(APITestCase):
+
+    def test_successful_registration(self):
+        """Тест успешной регистрации нового пользователя"""
+        print(Fore.CYAN + "Тест успешной регистрации нового пользователя")  # Заголовок теста в голубом цвете
+
+        data = {
+            "email": "topi768@inbox.ru",
+            "name": "John Doe",
+            "password": "SecureP@ss123",
+            "re_password": "SecureP@ss123",
+            "country": "USA",
+            "city": "New York",
+            "phone": "1234567890",
+            "date_of_birth": "1990-01-01"
+        }
+        response = self.client.post('/auth/users/', data, format='json')
+
+        # Печать результата с цветом
+        if response.status_code == status.HTTP_201_CREATED:
+            print(Fore.GREEN + "Регистрация прошла успешно!")
+        else:
+            print(Fore.RED + f"Ошибка регистрации: {response.status_code}")
+
+        # Проверка, что статус ответа 201 (создано)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- django_project/project/new_app/tests/test_basic.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class BasicTestCase(TestCase):
-    def test_addition(self):
-        """Проверка, что 2 + 2 = 4"""
-        self.assertEqual(2 + 2, 4)
